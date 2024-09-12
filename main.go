e0d4816bb36969a44c7a2d0daf36b2d41d5d36f7
joelcardoso95 <joelmarcos.cardoso@aol.com>
2024-09-12 16:05:49 -0300

    feat: reading DynamoDB Stream and starting new Step Functions Execution

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go/aws"
@@ -11,40 +12,64 @@ import (
 )
 
 const (
-	awsRegion = "us-west-2"
+	awsRegion = "us-east-1"
 )
+
+type User struct {
+	Username string `json:"userName"`
+	UserId   string `json:"userId"`
+}
 
 func main() {
 	// Create a new context
 	ctx := context.Background()
+	// Start the Lambda handler
+	log.Println("Starting Lambda Handler")
 	lambda.StartWithOptions(handler, lambda.WithContext(ctx))
 }
 
 func handler(ctx context.Context, events events.DynamoDBEvent) {
-	log.Println("Received events: ", events)
+	log.Println("Received events: ", events.Records)
 
 	// Create a new session
 	sess := session.Must(session.NewSession())
-
+	//
 	// Create SFN New Client
 	sfnClient := sfn.New(sess, aws.NewConfig().WithRegion(awsRegion))
-
+	//
 	// SFN Arn
-	stateMachinearn := "arn:aws:states:us-west-2:123456789012:stateMachine:HelloWorld-StateMachine"
+	stateMachinearn := "arn:aws:states:us-east-1:#yourAccount:stateMachine:poc"
+	//
+	for _, record := range events.Records {
+		log.Println("Processing Record: ", record.EventName)
 
-	// Input Payload JSON
-	input := `{"name": "John"}`
+		if record.EventName == "MODIFY" {
+			// reading the new image and setting user properties
+			newImage := record.Change.NewImage
+			user := User{
+				Username: newImage["user_name"].String(),
+				UserId:   newImage["user_id"].String(),
+			}
+			inputJson, err := json.Marshal(user)
+			if err != nil {
+				log.Println("Error marshalling NewImage to JSON: ", err)
+				panic(err)
+			}
+			input := string(inputJson)
+			log.Println("Input: ", input)
 
-	// Start Execution
-	startExecutionInput := &sfn.StartExecutionInput{
-		StateMachineArn: aws.String(stateMachinearn),
-		Input:           aws.String(input),
+			// Start Execution
+			startExecutionInput := &sfn.StartExecutionInput{
+				StateMachineArn: aws.String(stateMachinearn),
+				Input:           aws.String(input),
+			}
+
+			result, err := sfnClient.StartExecution(startExecutionInput)
+			if err != nil {
+				log.Println("Error starting execution: ", err)
+				panic(err)
+			}
+			log.Println("Execution Started: ", result)
+		}
 	}
-
-	result, err := sfnClient.StartExecution(startExecutionInput)
-	if err != nil {
-		log.Println("Error starting execution: ", err)
-		return
-	}
-	log.Println("Execution Started: ", result)
 }
